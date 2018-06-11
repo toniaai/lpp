bbe1c6209ab5eb6c34efb668713bd2c44d82de75
toniaai <aai9@alu.ua.es>
2018-06-11 14:50:18 +0200

    Ejercicio 1

--- Practica1.swift
+++ Practica1.swift
@@ -1,5 +1,38 @@
-var str = "Mensaje"
-str = "Hola mundo!"
-print(str)
+// Ejercicio 1
 
+func obtenerFrecuencias(respuestas: [Int]) -> [Int] {
+    var frecuencias = Array(repeating: 0, count: 10)
+    for puntuacion in respuestas {
+        frecuencias[puntuacion] += 1
+    }
+    return frecuencias
+}
+
+func mayorFrecuencia(frecuencias: [Int]) -> Int {
+    var mayor = frecuencias[0]
+    for frec in frecuencias[1..<frecuencias.count] {
+        if frec > mayor {
+            mayor = frec;
+        }
+    }
+    return mayor
+}
+
+func imprimir(frecuencias: [Int], maxAsteriscos: Int) {
+    let mayor = mayorFrecuencia(frecuencias: frecuencias)
+    var i = 0
+    for frec in frecuencias {
+        let numEscalado = frec * maxAsteriscos / mayor
+        print("\(i): " + String(repeating: "*", count: numEscalado))
+        i += 1
+    }
+}
+
+let respuestas = [0,0,1,1,2,1,2,3,5,1,2,2,2,6]
+print("Valores: \(respuestas)" )
+let frec = obtenerFrecuencias(respuestas: respuestas)
+print("Frecuencias: \(frec)")
+print("\nHistograma")
+print("----------")
+imprimir(frecuencias: frec, maxAsteriscos: 10)
 
