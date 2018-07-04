692ad97508ae617d50cc3b8793f1d64cdb0dd9f0
toniaai <aai9@alu.ua.es>
2018-07-04 19:39:27 +0200

    random updateado

--- scheme/ejersRandom.rkt
+++ scheme/ejersRandom.rkt
@@ -36,6 +36,7 @@
 
 (define arbolb-vacio '())
 
+;; sustituir en un arbol
 (define (arbol-sust arbol elem-old  elem-new)
   (construye-arbol (if (equal? (dato-arbol arbol) elem-old)
             elem-new
@@ -56,6 +57,7 @@
                        (dato-arbol arbol)) (map (lambda (x)
                                                   (arbol-sust-fos x elem-old elem-new)) (hijos-arbol arbol))))
 
+;; maximo de un arbol
 (define (max-arbol arbol)
   (max (dato-arbol arbol)
        (max-bosque (hijos-arbol arbol))))
@@ -63,7 +65,7 @@
 (define (max-bosque bosque)
   (if (null? bosque)
       0
-      (max (max-arbol (car bosque))
+      (max (max-arbol (car bosque)))))
 
            
 (define (max-arbol-fos arbol)
@@ -75,7 +77,7 @@
 
 (define arbolMax '(10 (20 (4) (3)) (12 (1) (2)) (15 (21) (2))))
 
-
+;; lista-nivel
 (define (lista-nivel-tree nivel arbol)
   (if (= nivel 0)
       (list (dato-arbol arbol))
@@ -95,3 +97,75 @@
 
 
 (define arbolNivel '(1 (2 (3 (4) (2)) (5)) (6 (7))))
+
+;; cumplen-pred arbol
+
+(define (cumplen-pred-arbol pred arbol)
+  (construye-arbol (if (pred (dato-arbol arbol))
+                       #t
+                       #f)
+                   (cumplen-pred-bosque pred (hijos-arbol arbol))))
+
+(define (cumplen-pred-bosque pred bosque)
+  (if (null? bosque)
+      '()
+      (cons (cumplen-pred-arbol pred (car bosque))
+            (cumplen-pred-bosque pred (cdr bosque)))))
+
+(define (cumplen-pred-arbol-fos pred arbol)
+  (construye-arbol (if (pred (dato-arbol arbol))
+                       #t
+                       #f)
+                   (map (lambda (x)
+                          (cumplen-pred-arbol-fos pred x)) (hijos-arbol arbol))))
+
+;; cuenta pares arbol
+(define (suma-parejas p1 p2)
+  (cons (+ (car p1) (car p2))
+        (+ (cdr p1) (cdr p2))))
+
+(define (cuenta-pares-impares-arbol arbol)
+  (suma-parejas (if (even? (dato-arbol arbol))
+         (cons 1 0)
+         (cons 0 1))
+     (cuenta-pares-impares-bosque (hijos-arbol arbol))))
+
+(define (cuenta-pares-impares-bosque bosque)
+  (if (null? bosque)
+      (cons 0 0)
+      (suma-parejas (cuenta-pares-impares-arbol (car bosque))
+         (cuenta-pares-impares-bosque (cdr bosque)))))
+
+
+(define (cuenta-pares-impares-arbol-fos arbol)
+  (fold-right (lambda (x y)
+                (suma-parejas x y)) (if (even? (dato-arbol arbol))
+                                        (cons 1 0)
+                                        (cons 0 1)) (map (lambda (x)
+                                                           (cuenta-pares-impares-arbol-fos x)) (hijos-arbol arbol))))
+
+;; diffs arbol
+(define arbolDiff1 '(1 (2 (3 (4) (40)) (5)) (6 (7))))
+(define arbolDiff2 '(1 (2 (10 (4) (2)) (21)) (6 (7))))
+
+(define (diff-arbol arbol1 arbol2)
+  (append (if (equal? (dato-arbol arbol1) (dato-arbol arbol2))
+              '()
+              (list (cons (dato-arbol arbol1) (dato-arbol arbol2))))
+          (diff-bosque (hijos-arbol arbol1) (hijos-arbol arbol2))))
+
+(define (diff-bosque bosque1 bosque2)
+  (if (null? bosque1)
+      '()
+      (append (diff-arbol (car bosque1) (car bosque2))
+              (diff-bosque (cdr bosque1) (cdr bosque2)))))
+
+(define (diff-arbol-fos arbol1 arbol2)
+  (fold-right append (if (equal? (dato-arbol arbol1) (dato-arbol arbol2))
+                         '()
+                         (list (cons (dato-arbol arbol1) (dato-arbol arbol2))))
+              (map (lambda (x y)
+                     (diff-arbol-fos x y)) (hijos-arbol arbol1) (hijos-arbol arbol2))))
+
+
+                    
